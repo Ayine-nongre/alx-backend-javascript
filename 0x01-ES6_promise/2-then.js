7f32fae4ddef9b7859cd7496e8657f1556163c3f
Ayine-nongre <eugeneatinbire@gmail.com>
2024-03-06 22:34:22 +0000

    Fixing err

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,16 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-  // eslint-disable-next-line no-unused-vars
-    .then((res) => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'Success',
-      };
-    })
-  // eslint-disable-next-line no-unused-vars
-    .catch((err) => {
-      console.log('Got a response from the API');
-      Error();
-    });
+    .then(() => ({
+      status: 200,
+      body: 'Success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }

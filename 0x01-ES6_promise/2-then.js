b48b55a0dc4409f1ee235b4369591ac606c2a286
Ayine-nongre <eugeneatinbire@gmail.com>
2024-03-06 22:40:04 +0000

    Fixing err

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => ({
-      status: 200,
-      body: 'Success',
-    }))
-    .catch(() => Error())
+        status: 200,
+        body: 'Success',
+      }))
+    .catch(() => Error(''))
     .finally(() => console.log('Got a response from the API'));
 }

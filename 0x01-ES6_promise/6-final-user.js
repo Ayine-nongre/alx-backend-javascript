017c1bb7cc691b63ec7a0ac89f49ef82903444b6
Ayine-nongre <eugeneatinbire@gmail.com>
2024-03-07 10:16:25 +0000

    Trying again

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([(signUpUser(firstName, lastName)), uploadPhoto(filename)])
-    .then((values) => values);
+export default async function handleProfileSignup(firstName, lastName, filename) {
+  const user = await signUpUser(firstName, lastName).then((res) => ({
+    status: 'fulfilled',
+    value: res,
+  }));
+
+  const photo = await uploadPhoto(filename).then((res) => ({
+    status: 'rejected',
+    value: res,
+  }));
+
+  return [user, photo];
 }

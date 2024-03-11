011f8e28921e95d4cbaa3946b0f3cc1be10cce06
Ayine-nongre <eugeneatinbire@gmail.com>
2024-03-11 23:09:11 +0000

    Fixing task 1 output err

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,7 @@
 export default function getListStudentIds(arr) {
   const newArr = [];
   if (Array.isArray(arr)) {
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < arr.length; i++) {
-      newArr.push(arr[i].id);
-    }
+    arr.map((value) => newArr.push(value.id));
   }
 
   return newArr;

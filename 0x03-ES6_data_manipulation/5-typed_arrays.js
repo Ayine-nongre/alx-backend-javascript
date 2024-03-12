1d3b817399a5c33b4177f7e3a207fb27cdf14a2d
Ayine-nongre <eugeneatinbire@gmail.com>
2024-03-12 12:54:01 +0000

    Fixing err

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,5 @@
 export default function createInt8TypedArray(length, position, value) {
-  if (positon >= length || position < 0) {
+  if (position >= length || position < 0) {
     throw new Error('Position outside range');
   }
 
